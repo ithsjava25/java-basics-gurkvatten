f16fea15241d7b4caf6800a21f274b7db338c13a
Johan Karlsson <93186588+gurkvatten@users.noreply.github.com>
2025-09-17 13:11:25 +0200

    simple test to write out what ElpriserApi shows

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,5 +5,10 @@ import com.example.api.ElpriserAPI;
 public class Main {
     public static void main(String[] args) {
         ElpriserAPI elpriserAPI = new ElpriserAPI();
+
+
+        for (String arg : args) {
+            System.out.println("Antal argument" + args.length);
+        }
     }
 }
